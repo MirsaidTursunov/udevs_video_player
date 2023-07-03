ca2c594fe50d630db1253ec7bf7d96673768280b
Mirsaid <mirsaidtursunov7@gmail.com>
2023-07-03 16:00:02 +0500

    channels change added and some changes

---
+++ android/src/main/kotlin/uz/udevs/udevs_video_player/adapters/ChannelsPagerAdapter.kt
@@ -0,0 +1,82 @@
+package uz.udevs.udevs_video_player.adapters
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.MotionEvent
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager2.widget.ViewPager2
+import uz.udevs.udevs_video_player.R
+import uz.udevs.udevs_video_player.models.TvChannel
+
+class ChannelsPagerAdapter(
+    var viewPager: ViewPager2,
+    var context: Context,
+    var channels: List<TvChannel>,
+    var seasonsIndex: Int,
+    var episodeIndex: Int,
+    var onClickListener: OnClickListener
+) :
+    RecyclerView.Adapter<ChannelsPagerAdapter.Vh>() {
+    inner class Vh(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val rv: RecyclerView
+
+        init {
+            rv = itemView.findViewById(R.id.episodes_rv)
+            rv.addOnItemTouchListener(object : RecyclerView.OnItemTouchListener {
+                var lastX = 0
+                override fun onInterceptTouchEvent(rv: RecyclerView, e: MotionEvent): Boolean {
+                    when (e.action) {
+                        MotionEvent.ACTION_DOWN -> lastX = e.x.toInt()
+                        MotionEvent.ACTION_MOVE -> {
+                            val isScrollingRight = e.x < lastX
+                            viewPager.isUserInputEnabled =
+                                isScrollingRight && (rv.layoutManager as LinearLayoutManager).findLastCompletelyVisibleItemPosition() == rv.adapter!!.itemCount - 1 ||
+                                        !isScrollingRight && (rv.layoutManager as LinearLayoutManager).findFirstCompletelyVisibleItemPosition() == 0
+                        }
+                        MotionEvent.ACTION_UP -> {
+                            lastX = 0
+                            viewPager.isUserInputEnabled = true
+                        }
+                    }
+                    return false
+                }
+
+                override fun onTouchEvent(rv: RecyclerView, e: MotionEvent) {}
+                override fun onRequestDisallowInterceptTouchEvent(disallowIntercept: Boolean) {}
+            })
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Vh {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.episode_page, parent, false)
+        return Vh(view)
+    }
+
+    override fun onBindViewHolder(holder: Vh, @SuppressLint("RecyclerView") position: Int) {
+        val layoutManager =
+            LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
+        holder.rv.layoutManager = layoutManager
+//        holder.rv.adapter = EpisodesRvAdapter(
+//            context,
+//            seasonsIndex == position,
+//            episodeIndex,
+//            channels[position].id,
+//            object : EpisodesRvAdapter.OnClickListener {
+//                override fun onClick(episodeIndex: Int) {
+//                    onClickListener.onClick(episodeIndex, position)
+//                }
+//            })
+    }
+
+    override fun getItemCount(): Int {
+        return channels.size
+    }
+
+    interface OnClickListener {
+        fun onClick(epIndex: Int, seasIndex: Int)
+    }
+}

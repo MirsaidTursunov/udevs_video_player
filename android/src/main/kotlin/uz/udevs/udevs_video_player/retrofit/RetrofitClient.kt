a121d0d7bdc7d94b5fa0c7892dd1fec1d06cee86
Abdurahmon727 <abdurahmon727.uz@gmail.com>
2024-08-01 10:37:15 +0500

    fix: retrofit converter

--- android/src/main/kotlin/uz/udevs/udevs_video_player/retrofit/RetrofitClient.kt
+++ android/src/main/kotlin/uz/udevs/udevs_video_player/retrofit/RetrofitClient.kt
@@ -1,12 +1,14 @@
 package uz.udevs.udevs_video_player.retrofit
 
+import com.google.gson.GsonBuilder
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
     fun getRetrofit(baseUrl: String): Retrofit {
+        val gson = GsonBuilder().setLenient().create()
         return Retrofit.Builder()
-            .addConverterFactory(GsonConverterFactory.create())
+            .addConverterFactory(GsonConverterFactory.create(gson))
             .baseUrl(baseUrl)
             .build()
     }

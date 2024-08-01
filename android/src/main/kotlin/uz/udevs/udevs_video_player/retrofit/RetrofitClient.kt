dc5dd3e441a7ebd1576a5a0cb44b4166d135068b
Abdurahmon727 <abdurahmon727.uz@gmail.com>
2024-08-01 10:50:55 +0500

    feat: add logging

--- android/src/main/kotlin/uz/udevs/udevs_video_player/retrofit/RetrofitClient.kt
+++ android/src/main/kotlin/uz/udevs/udevs_video_player/retrofit/RetrofitClient.kt
@@ -1,15 +1,22 @@
 package uz.udevs.udevs_video_player.retrofit
 
 import com.google.gson.GsonBuilder
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 object RetrofitClient {
     fun getRetrofit(baseUrl: String): Retrofit {
         val gson = GsonBuilder().setLenient().create()
-        return Retrofit.Builder()
-            .addConverterFactory(GsonConverterFactory.create(gson))
-            .baseUrl(baseUrl)
-            .build()
+        val logging = HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+        val client =
+            OkHttpClient.Builder().addInterceptor(logging).readTimeout(30, TimeUnit.SECONDS)
+                .writeTimeout(30, TimeUnit.SECONDS).callTimeout(30, TimeUnit.SECONDS).build()
+        return Retrofit.Builder().addConverterFactory(GsonConverterFactory.create(gson))
+            .baseUrl(baseUrl).client(client).build()
     }
 }

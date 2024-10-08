7b9d1d0c62d40cebfd812e1903dfe2c9ae7a1760
Abdurahmon727 <abdurahmon727.uz@gmail.com>
2024-10-08 14:21:51 +0500

    fix: remove ads

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -291,27 +291,27 @@ class _MainPageState extends State<MainPage> {
   Future<void> playLiveVideo() async {
     try {
       final s = await _udevsVideoPlayerPlugin.playLiveVideo(
-            playerConfig: LivePlayerConfiguration(
+            playerConfig: const LivePlayerConfiguration(
               videoUrl:
                   'https://cdn.uzd.udevs.io/uzdigital/videos/bbc795b69faf3c4d1063777f2b220144/master.m3u8',
               sessionId: '',
               skipText: 'Skip Ad',
-              advertisement: AdvertisementResponse(
-                id: 'f07d0dab-8466-45b5-8b10-74e264af8e8e',
-                bannerImage: BannerImage(
-                  mobileImage:
-                      'https://cdn.uzd.udevs.io/uzdigital-images-compressed/images/c30549a1-bda9-4f15-a7db-fe55ba816cf8.jpg',
-                  webImage:
-                      'https://cdn.uzd.udevs.io/uzdigital-images-compressed/images/49b46d84-f0f4-4bea-80df-69f10e51d15d.jpg',
-                  tvImage:
-                      'https://cdn.uzd.udevs.io/uzdigital-images-compressed/images/7ca0217a-b2ec-414e-aca8-849a8702a383.jpg',
-                ),
-                skipDuration: 5,
-                link: 'https://t.me/uzdigital_tv_bot/app',
-                video:
-                    'https://test.cdn.uzdigital.tv/uzdigital/images/8cb238ae-bd88-4734-84f5-6bedc0f4c194.mp4'
-              ),
-              baseUrl: '',
+              // advertisement: AdvertisementResponse(
+              //   id: 'f07d0dab-8466-45b5-8b10-74e264af8e8e',
+              //   bannerImage: BannerImage(
+              //     mobileImage:
+              //         'https://cdn.uzd.udevs.io/uzdigital-images-compressed/images/c30549a1-bda9-4f15-a7db-fe55ba816cf8.jpg',
+              //     webImage:
+              //         'https://cdn.uzd.udevs.io/uzdigital-images-compressed/images/49b46d84-f0f4-4bea-80df-69f10e51d15d.jpg',
+              //     tvImage:
+              //         'https://cdn.uzd.udevs.io/uzdigital-images-compressed/images/7ca0217a-b2ec-414e-aca8-849a8702a383.jpg',
+              //   ),
+              //   skipDuration: 5,
+              //   link: 'https://t.me/uzdigital_tv_bot/app',
+              //   video:
+              //       'https://test.cdn.uzdigital.tv/uzdigital/images/8cb238ae-bd88-4734-84f5-6bedc0f4c194.mp4'
+              // ),
+              baseUrl: 'https://api.spec.uzd.udevs.io/v1/',
               qualityText: 'Качество',
               speedText: 'Скорость',
               title: 'S1 E1  "Женщина-Халк: Адвокат" ',
@@ -324,7 +324,7 @@ class _MainPageState extends State<MainPage> {
               selectChannelIndex: 0,
               ip: '',
               tvCategories: [
-                const TvCategories(
+                TvCategories(
                   id: '',
                   title: 'Все',
                   tvChannels: [
